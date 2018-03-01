1ca310031c87de2ce3ff385b18f4a358784fc1d1
Matt Sampson <matthew@therubythree.com>
2018-03-01 12:28:50 -0800

    Implemented EDIT and UPDATE routes/methods

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,24 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+    # sets portfolio_item to the Portfolio with the id provided in the URL
+    # http://localhost:3000/portfolios/2/edit
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
+        format.html { redirect_to portfolios_path, notice: "Record updated" }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
 
