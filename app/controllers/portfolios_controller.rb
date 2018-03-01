5b49ef1dd10b5121270b05923633c39c1f423022
Matt Sampson <matthew@therubythree.com>
2018-03-01 11:51:06 -0800

    Added NEW and CREATE routes for Portfolios/

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,21 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        # Sends the user to the index page -> /portfolios => portfolios#index
+        format.html { redirect_to portfolios_path, notice: "Your portfolio has been created "}
+      else
+        format.html {render :new }
+      end
+    end
+  end
 end
