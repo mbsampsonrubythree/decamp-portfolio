b138d60c29235790cdc3c05eb4f4ef82598f3e2e
Matt Sampson <matthew@therubythree.com>
2018-03-01 15:40:35 -0800

    Configured custom routes for pages#about and pages#contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
 
-  get 'pages/about'
+  get 'about-us', to: 'pages#about'
+  # Navigating to:    localhost:3000/about-us
+  # renders.......    /view/pages/about.html.erb
 
-  get 'pages/contact'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
+
+  root to: 'pages#home'
+  # Navigating to:    localhost:3000/
+  # renders.......    /view/pages/home.html.erb
+
 end
