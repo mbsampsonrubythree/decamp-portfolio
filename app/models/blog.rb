bf15eb44799c52db16ba51850943cb6122e4b943
Matt Sampson <matthew@therubythree.com>
2018-03-05 11:02:50 -0800

    Integrated friendly_id and friendly routes for blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  # Takes the blog.title and automatically generates a slug for that title 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  # Creates a custom route so that the default 'portfolio/:id' link becomes
+  # 'portfolio/:id'
+  # This change makes more logical sense since we're showing a singular
+  # portfolio item when we navigate to through this route.
+
 
   get 'about-us', to: 'pages#about'
   # Navigating to:    localhost:3000/about-us
@@ -12,5 +19,4 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # Navigating to:    localhost:3000/
   # renders.......    /view/pages/home.html.erb
-
 end
