0d1032f64884cf1a50fcb2f1982691b520a9d421
Matt Sampson <matthew@therubythree.com>
2018-03-12 10:55:04 -0700

    Implemented helper methods for login and page greetings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,15 @@ module ApplicationHelper
       link_to "Logout", destroy_user_session_path, method: :delete
     else
       (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe + 
+      "<br>".html_safe +
       (link_to "Login", new_user_session_path)
+    end
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thank you for visiting me from #{session[:source]}. This is the #{layout_name} layout."
+      content_tag(:p, greeting, class: "source-greeting")
     end
   end
 end
