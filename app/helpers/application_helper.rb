25a7111fa6900e4091551a29fc30dfde1e8173a4
Matt Sampson <matthew@therubythree.com>
2018-03-13 11:33:03 -0700

    Added Bootstrap styles for contact and about pages. Fixed login_helper to properly display nav links on the navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,12 @@ module ApplicationHelper
     "<p>This is a helper!</p>".html_safe
   end
 
-  def login_container_helper
+  def login_container_helper(class_name)
     if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: class_name
     else
-      (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path)
+      (link_to "Register", new_user_registration_path, class: class_name) +
+      (link_to "Login", new_user_session_path, class: class_name)
     end
   end
 
