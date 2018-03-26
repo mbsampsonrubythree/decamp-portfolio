fe46c4a695adcc8e66e3f15a88ae0d9c98e56e3f
Matt Sampson <matthew@therubythree.com>
2018-03-26 10:52:58 -0700

    Modified login_helper to use Bootstrap button-group class when displaying login options on blog layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,19 @@ module ApplicationHelper
     "<p>This is a helper!</p>".html_safe
   end
 
-  def login_container_helper class_name = ''
+  def login_container_helper class_name = '', button_group = false
     if current_user.is_a?(User)
       link_to "Logout", destroy_user_session_path, method: :delete, class: class_name
     else
-      (link_to "Register", new_user_registration_path, class: class_name) +
-      (link_to "Login", new_user_session_path, class: class_name)
+      if button_group == true
+        ('<div class="btn-group" role="group" aria-label="Basic Example">').html_safe +
+        (link_to "Register", new_user_registration_path, class: "btn btn-secondary") +
+        (link_to "Login", new_user_session_path, class: "btn btn-secondary") +
+        ('</div>').html_safe
+      else
+        (link_to "Register", new_user_registration_path, class: class_name) +
+        (link_to "Login", new_user_session_path, class: class_name)
+      end
     end
   end
 
